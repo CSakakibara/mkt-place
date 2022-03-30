776f98dfe4f800c2a7d0d50850bfb9b74cbfdecd
Christopher Jun Maruyama Sakakibara <cj-sakakib@hotmail.com>
2022-03-29 22:18:16 -0300

    checkout page ui

---
+++ pages/checkoutPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <HeaderComponent />
+    <CheckoutForm />
+    <FooterComponent />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  computed: {
+    cartItems() {
+      return this.$store.state.shop.cart.cartItems
+    },
+  },
+  created() {
+    if (!this.cartItems.length) this.$router.push({ path: '/' })
+  },
+})
+</script>
+
+this.$router.push({path: "panel"})
