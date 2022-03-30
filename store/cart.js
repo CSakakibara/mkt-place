03399d4ae0f68abf5d58f52d542f8abc7375c548
Christopher Jun Maruyama Sakakibara <cj-sakakib@hotmail.com>
2022-03-29 22:21:40 -0300

    cart and input store

---
+++ store/cart.js
@@ -0,0 +1,49 @@
+export const getters = {
+  totalQuantity: (state) => {
+    return state.cartItems.reduce((sum, item) => {
+      return sum + item.quantity
+    }, 0)
+  },
+  totalPrice: (state) => {
+    return state.cartItems.reduce((sum, item) => {
+      return sum + item.quantity * item.price
+    }, 0)
+  },
+  financedTotalPrice: (state) => {
+    return state.cartItems.reduce((sum, item) => {
+      return sum + item.quantity * item.price // to-do: implement interest
+    }, 0)
+  },
+}
+
+export const mutations = {
+  addItem(state, item) {
+    const itemIndex = state.cartItems.findIndex((i) => i.id === item.id)
+    if (itemIndex === -1) {
+      state.cartItems.push({ ...item, quantity: 1 })
+      return
+    }
+    state.cartItems[itemIndex].quantity++
+  },
+  removeItem(state, item) {
+    const itemIndex = state.cartItems.findIndex((i) => i.id === item.id)
+    if (itemIndex === -1) return
+    state.cartItems.splice(itemIndex, 1)
+  },
+  subtractItem(state, item) {
+    const itemIndex = state.cartItems.findIndex((i) => i.id === item.id)
+    if (itemIndex === -1) return
+    if (state.cartItems[itemIndex].quantity <= 1) {
+      state.cartItems.splice(itemIndex, 1)
+      return
+    }
+    state.cartItems[itemIndex].quantity--
+  },
+  removeAll(state) {
+    state.cartItems = []
+  },
+}
+
+export const state = () => ({
+  cartItems: [],
+})
---
+++ store/inputs.js
@@ -0,0 +1,21 @@
+export const state = () => ({
+  searchTerm: '',
+  bannersIndex: 0,
+})
+
+export const mutations = {
+  setSearchTerm(state, text) {
+    state.searchTerm = text
+  },
+  changeToNextBanner(state, length) {
+    const maxIndex = length - 1
+    if (state.bannersIndex < maxIndex) {
+      state.bannersIndex++
+      return
+    }
+    state.bannersIndex = 0
+  },
+  changeBanner(state, position) {
+    state.bannersIndex = position
+  },
+}
